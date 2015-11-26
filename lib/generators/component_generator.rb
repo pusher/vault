9e0b2349dcb6ddc559bcfeb533ad08276110a70e
Alex Pate <alex.pate@pusher.com>
2015-11-26 13:01:03 +0000

    Created generator

---
+++ lib/generators/component_generator.rb
@@ -0,0 +1,11 @@
+class ComponentGenerator < Rails::Generators::NamedBase
+  desc "This generator will create the relevant Sass and html files needed for a new component"
+  def create_new_component
+    @component_name = name.split('/').last
+    @component_path = name.sub!(@component_name, '')
+
+    create_file "app/assets/stylesheets/components/#{@component_path}/_#{@component_name}.scss", ""
+    append_file "app/assets/stylesheets/application.scss", "\n@import 'components/#{@component_path}#{@component_name};'"
+    create_file "app/views/components/#{@component_path}/_#{@component_name}.html.erb", ""
+  end
+end
