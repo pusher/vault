d0224ece4fb842658d022a127f29ecdfc59bbfca
Alex Pate <alex.pate@pusher.com>
2015-11-26 10:44:42 +0000

    Sassified. Added example elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def render_source args={}
+      @html_encoder ||= HTMLEntities.new
+      raw(@html_encoder.encode(render args))
+  end
+
+  def highlight(&block)
+      formatter = Rouge::Formatters::HTML.new(wrap: false)
+      lexer = Rouge::Lexers::HTML.new
+
+      content = capture(&block)
+      raw '<pre class="syntax">' + formatter.format(lexer.lex(content)) + '</pre>'
+    end
 end
